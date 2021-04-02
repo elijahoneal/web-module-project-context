e8b22d73a7d46b0967c7c82ff93dc0f21f08d2cc
Elijah <elijahoneal@yahoo.com>
2021-04-01 21:07:28 -0400

    Add CartContext refactor Nav + Cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from './contexts/ProductContext'
+import CartContext from './contexts/CartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -18,17 +19,19 @@ function App() {
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={ {products, addItem} }>
-				<Navigation cart={cart} />
+			<ProductContext.Provider value = { {products, addItem} }>
+				<CartContext.Provider value = {cart}>
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products/>
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products/>
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
+					<Route path="/cart">
+						<ShoppingCart />
 				</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
